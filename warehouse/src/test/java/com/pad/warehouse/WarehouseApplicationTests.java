577a7c6d69adca5dce32c4430eaf6609f7477ab8
jacek <jacekpadyjasek@gmail.com>
2023-06-25 02:03:25 +0200

    first

---
+++ warehouse/src/test/java/com/pad/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pad.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
