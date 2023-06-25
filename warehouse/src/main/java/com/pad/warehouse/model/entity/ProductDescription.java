cc7453de2ac8967df498612daf178d132ebce673
jacek <jacekpadyjasek@gmail.com>
2023-06-26 00:45:43 +0200

    product

--- warehouse/src/main/java/com/pad/warehouse/model/entity/ProductDescription.java
+++ warehouse/src/main/java/com/pad/warehouse/model/entity/ProductDescription.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,6 +19,11 @@ public class ProductDescription {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product productId;
+
 
     @Column(name = "DESCRIPTION")
     private String productDescription; 
