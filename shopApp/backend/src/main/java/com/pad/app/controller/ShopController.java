16766a1c79e3503e5bc940c3858339d5b01d466e
jacpad <jacekpadyjasek@gmail.com>
2023-09-03 19:17:22 +0200

    webClient and caching

--- shopApp/backend/src/main/java/com/pad/app/controller/ShopController.java
+++ shopApp/backend/src/main/java/com/pad/app/controller/ShopController.java
@@ -1,11 +1,20 @@
 package com.pad.app.controller;
 
 import com.pad.app.model.Order;
+import com.pad.app.model.Product;
+import com.pad.app.model.ProductOrder;
+import com.pad.app.model.enums.DeliveryMethodEnum;
 import com.pad.app.service.OrderService;
-import com.pad.app.service.TestService;
+import com.pad.app.service.ProductService;
+import com.pad.warehouse.swagger.model.ProductList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("")
@@ -15,24 +24,44 @@ public class ShopController {
 
     private final OrderService orderService;
 
+    private final ProductService productService;
 
-    @PostMapping("/order")
-    public void makeOrder(@RequestBody Order order) {
-        orderService.makeOrder(order);
+//    @PostMapping("/order")
+    @GetMapping("/order")
+//    @RequestBody Order order
+    public void makeOrder() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("123");
+        ProductOrder productOrder = new ProductOrder();
+        productOrder.setProduct(product);
+        productOrder.setQuantityBought(4);
+        Product product1 = new Product();
+        product1.setId(4L);
+        ProductOrder productOrder1 = new ProductOrder();
+        productOrder1.setProduct(product1);
+        productOrder1.setQuantityBought(99);
+        List<ProductOrder> orders = new ArrayList<>();
+        orders.add(productOrder);
+        orders.add(productOrder1);
+        Order order1 = new Order();
+        order1.setProducts(orders);
+        order1.setDeliveryMethod(DeliveryMethodEnum.POST);
+        orderService.makeOrder(order1);
 // make order and descrease number of products in warehouse and send order to other service
     }
 
     @GetMapping("/products")
-    public void getProducts() {
-// get cached products or make call to warehouse
+    public ResponseEntity<List<ProductList>> getProducts() {
+// get cached products or make call to warehouse to check if ok
+        log.info("getting products");
+        List<ProductList> products = productService.getProducts();
+        return new ResponseEntity<>(products, null, 200);
     }
 
     @GetMapping("/products/{id}")
     public void getDetails() {
 // get all product details when user clicks on product?
     }
-
-
-
 
 }
--- shopApp/backend/src/main/java/com/pad/app/service/ProductService.java
+++ shopApp/backend/src/main/java/com/pad/app/service/ProductService.java
@@ -1,25 +1,41 @@
 package com.pad.app.service;
 
+import com.pad.warehouse.swagger.model.ProductList;
+import com.pad.warehouse.swagger.model.ProductsResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.pad.app.model.Product;
+import java.util.List;
+
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ProductService {
+
+    @Value("${product.get-all.uri}")
+    private String PRODUCT_URI;
+
+    private final WebClientService webClientService;
     
-    public void getProducts() {
-        // get product from warehouse and cache it?
+    public List<ProductList> getProducts() {
+//        TODO cache caffeine it
+        List<ProductList> productLists = retrieveProducts();
+        return productLists;
+    }
+
+    private List<ProductList> retrieveProducts() {
+        ProductsResponse products = webClientService.webClientGet(PRODUCT_URI, ProductsResponse.class);
+        log.info("retrieved prodcuts: {}", products);
+        return products.getProducts();
     }
 
     public void getProductDetails() {
         // get details of product
     }
 
-    public void cleanCache() {
-        // clean cache with scheduler
-    }
 
     public boolean isProductAvailable(Long id) {
         // check if available and update cached value?
