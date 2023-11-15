f728ab7a42975df429263831237d9481723f1599
JacekPad <jacekpadyjasek@gmail.com>
2023-11-15 01:40:34 +0100

    securing orderApp

--- orderApp/src/main/java/com/pad/orderapp/config/SecurityConfig.java
+++ orderApp/src/main/java/com/pad/orderapp/config/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.pad.orderapp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityChain(HttpSecurity http) throws Exception{
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//        TODO add roles and role checks
+//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new );
+        http
+                .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requestManager -> {
+                    requestManager.requestMatchers("/**").authenticated();
+//                            .anyRequest().hasAnyRole();
+//                    for testing
+//                    requestManager.requestMatchers("/**").permitAll();
+                })
+                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+        return http.build();
+    }
+
 }
